680d401ecf12e8687fbf07405d4cab5342633089
amh6281 <amh6281@naver.com>
2023-06-26 09:45:57 +0900

    Home design 2022.06.26

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,17 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        하루 이야기
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">Daystory</span>
+      </h1>
+      <p className="desc text-center">
+        하루 이야기는 오늘 하루 다짐 혹은 오늘 있었던 당신만의 따스한 이야기를
+        짧게 기록하는 플랫폼입니다.
+      </p>
+    </section>
+  );
 };
 
 export default Home;
