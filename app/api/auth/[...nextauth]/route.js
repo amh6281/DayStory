43b39f9230662dae044afbe74d8cbdb23d1f699e
amh6281 <amh6281@naver.com>
2023-06-29 11:22:04 +0900

    SignIn api 생성 2022.06.29

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -13,6 +14,19 @@ const handler = NextAuth({
   async signIn({ profile }) {
     try {
       await connectToDB();
+
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
     } catch (err) {
       console.log(err);
       return false;
