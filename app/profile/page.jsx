899362b1b2142888847f87155a4a7b37e18ed8a8
amh6281 <amh6281@naver.com>
2023-07-02 09:17:33 +0900

    Profile page get posts api 호출 2022.07.02
    useSession은 비동기 hook이므로, useEffect의 의존성배열에 session.user.id를 추가한다.

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,6 +6,20 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]); // useSession은 비동기. 따라서, session값을 받아온 후에 호출되어야 하기 때문에 의존성배열에 session.user.id
+
   const handleEdit = async () => {};
   const handleDelete = async () => {};
 
@@ -13,7 +27,7 @@ const MyProfile = () => {
     <Profile
       name="My"
       desc="Welcome"
-      data={[]}
+      data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
