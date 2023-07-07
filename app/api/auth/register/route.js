538ee0dffe810367d1971837099eaeca988423d5
amh6281 <amh6281@naver.com>
2023-07-07 11:38:49 +0900

    Register api 생성 2022.07.07

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,26 @@
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+import bcrypt from "bcryptjs";
+
+export const POST = async (request) => {
+  const { name, email, password } = await request.json();
+
+  await connectToDB();
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    return new Response("계정이 생성되었습니다.", { status: 201 });
+  } catch (err) {
+    return new Response(err.message, {
+      status: 500,
+    });
+  }
+};
